2c9bf2e92eedc22e41d49c541087316683b67f15
Родион Косолапкин <kosolapkinr@gmail.com>
2025-04-24 22:36:53 +0400

    fix

--- src/main/kotlin/Comissions.kt
+++ src/main/kotlin/Comissions.kt
@@ -1,52 +1,78 @@
 package ru.netology
 
 fun main() {
-    println(calculateCommision("Visa", 50_000, 34_000))
-    println(calculateCommision("Мир", 80_000, 45_000))
-    println(calculateCommision("MasterCard", 550_000, 120_000))
-    println(calculateCommision("NoNameCard", 20_000, 35_000))
+    println("Тест 1: Visa " + calculateCommission("Visa", 50_000, 34_000))
+    println("Тест 2: Visa " + calculateCommission("Visa", 100_000, 200_000)) //превышен дневной лимит
+    println()
+
+    println("Тест 1: Мир " + calculateCommission("Мир", 80_000, 45_000))
+    println("Тест 2: Мир " + calculateCommission("Мир", 550_000, 55_000)) //превышен месячный лимит
+    println()
+
+    println("Тест 1: MasterCard " + calculateCommission("MasterCard", 15_000, 25_000)) // Порог не превышен
+    println("Тест 2: MasterCard " + calculateCommission("MasterCard", 74_000, 2_000)) // Превышение происходит при переводе
+    println("Тест 3: MasterCard " + calculateCommission("MasterCard", 75_000, 2_000)) // Лимит исчерпан
+    println()
+
+    println("Тест 1: NoNameCard " + calculateCommission("NoNameCard", 20_000, 35_000))
 }
 
-fun calculateCommision(
-    cardtype:String,
-    previousTransfer: Int,
-    transferAmount: Int
+fun calculateCommission(
+    cardtype:String = "Visa",
+    previousTransfer: Int = 0,
+    transferAmount: Int = 10_000
 ): Int? {
 
     val dayLimit = 150_000
     val monthLimit = 600_000
     val freeLimitOnMonth = 75_000
-    val minVisaCommision = 35
+    val minVisaCommission = 35
+    val masterCardBase = 20
+    val masterCardPercent = 0.006
+    val visaPercent = 0.0075
 
     if (transferAmount > dayLimit) {
-        println("Превышен суточный лимит перевода")
+        println("Превышен суточный лимит $dayLimit руб.")
         return null
     }
 
     if (previousTransfer + transferAmount > monthLimit) {
-        println("Превышен месячный лимит перевода")
+        println("Превышен месячный лимит $monthLimit руб.")
         return null
     }
 
     return when (cardtype) {
         "MasterCard" -> {
+            val total = previousTransfer + transferAmount
 
-            val amountLimitPlus = (previousTransfer + transferAmount) - freeLimitOnMonth
+//            if (amountLimitPlus <= freeLimitOnMonth) {
+//                return 0
+//            } else {
+//                val commission = (amountLimitPlus * masterCardPercent).toInt() + masterCardBase
+//                commission
+//            }
 
-            if (amountLimitPlus <= 0) {
-                return 0
-            } else {
-                val commision = (amountLimitPlus*0.006).toInt() + 20
-                commision
+            when {
+                // Порог не превышен
+                total <= freeLimitOnMonth -> 0
+
+                // Превышение происходит при переводе
+                previousTransfer < freeLimitOnMonth -> {
+                    val freeRemaining = freeLimitOnMonth - previousTransfer
+                    val chargedAmount = transferAmount - freeRemaining
+                    (chargedAmount * masterCardPercent).toInt() + masterCardBase
+                }
+                // Лимит исчерпан
+                else -> (transferAmount * masterCardPercent).toInt() + masterCardBase
             }
         }
 
         "Visa" -> {
-            val commision = (transferAmount * 0.0075).toInt()
-            if (commision < minVisaCommision) {
-                return minVisaCommision
+            val commission = (transferAmount * visaPercent).toInt()
+            if (commission <= minVisaCommission) {
+                return minVisaCommission
             } else {
-                commision
+                commission
             }
         }
 
@@ -55,7 +81,7 @@ fun calculateCommision(
         }
 
         else -> {
-            println("Неизвестный тип карты")
+            println("Неизвестный тип карты '$cardtype'")
             return null
         }
     }
--- src/main/kotlin/online.kt
+++ src/main/kotlin/online.kt
@@ -8,15 +8,17 @@ fun main() {
     agoToText(86400)   // был(а) вчера
     agoToText(172800)  // был(а) позавчера
     agoToText(259200)  // был(а) давно
+    agoToText(43200)  // был(а) давно
+    agoToText(720)  // был(а) давно
 }
 
 fun agoToText(time: Int) {
     val text = when (time) {
         in 0..60 -> "Был(а) только что"
         in 61..(60*59) -> "Был(а) ${getMinutes(time/60)} назад"
-        in (60*60)..(24*60*60) -> "Был(а) ${getHours(time/(60*60))} назад"
-        in (24*60*60+1)..(2*24*60*60) -> "Был(а) вчера"
-        in (2*24*60*60+1)..(3*24*60*60) -> "Был(а) позавчера"
+        in (60*60)..(24*60*59) -> "Был(а) ${getHours(time/(60*60))} назад"
+        in (24*60*60)..(2*24*60*59) -> "Был(а) вчера"
+        in (2*24*60*60)..(3*24*60*59) -> "Был(а) позавчера"
         else -> "Был(а) давно"
     }
     println(text)
@@ -24,6 +26,8 @@ fun agoToText(time: Int) {
 
 fun getMinutes(minutes:Int): String {
     return when {
+        minutes % 100 in 11..14 -> "$minutes минут"
+
         minutes % 10 == 1 -> "$minutes минуту"
         minutes % 10 in 2..4 -> "$minutes минуты"
         else -> "$minutes минут"
@@ -33,6 +37,8 @@ fun getMinutes(minutes:Int): String {
 
 fun getHours(hours: Int): String {
     return when {
+        hours % 100 in 11..14 -> "$hours часов"
+
         hours % 10 == 1 -> "$hours час"
         hours % 10 in 2..4 -> "$hours часа"
         else -> "$hours часов"
